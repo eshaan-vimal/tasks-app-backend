b302cf5c4dc6169901afc8535275c67f9f1b4567
eshaan-vimal <evk2.development@gmail.com>
2025-03-29 01:33:49 +0530

    update drizzle.config.ts for production

--- src/drizzle.config.ts
+++ src/drizzle.config.ts
@@ -5,23 +5,12 @@ import path from 'path';
 
 dotenv.config({path: path.resolve(__dirname, "../.env")});
 
-// console.log(process.env.DB_PASSWORD);
-// console.log(process.env.DB_HOST);
-// console.log(process.env.DB_NAME);
-// console.log(process.env.DB_USER);
-// console.log(process.env.DB_PORT);
-
-
 export default defineConfig({
     dialect: "postgresql",
     schema: "./models/*.ts",
     out: "./drizzle",
     dbCredentials: {
-        host: "localhost",
-        port: Number(process.env.DB_HOST_PORT) || 5432,
-        database: process.env.DB_NAME!,
-        user: process.env.DB_USER!,
-        password: process.env.DB_PASSWORD!,
-        ssl: false,
+        url: process.env.DB_URL!,
+        ssl: true,
     },
 });
